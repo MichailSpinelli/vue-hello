57b1c9195aa9ec5b42ce48ec5ea12acd2e73c7ec
Michail Spinelli <michailspinelli96@gmail.com>
2024-01-07 17:16:31 +0100

    meta

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 
+// creo una nuova istanza di Vue nella variabile createApp
+const { createApp } = Vue;
+
+// popoliamo la nostra istanza di Vue con i dati che vogliamo inserire
+createApp({
+    data() {
+        return {
+            title : `Lorem  ndiqodnqwi onqwdio qnwdionqwdon`,
+            imageSrc : 'Lorem  ndiqodnqwi onqwdio qnwdionqwdon ',
+        }
+    },
+
+    methods: {
+
+    }
+
+
+}).mount('#app');
